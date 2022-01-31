7917bb9435544a3455b9d920fd43c3ec3915f086
csagar131 <chouhansagar131@gmail.com>
2022-01-31 11:35:43 +0530

    update user.js controller

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/user")
-
+const Order = require("../models/order")
 
 // middleware to attach user to req body as profile
 exports.getUserById = (req, res, next, id) => {
@@ -9,7 +9,6 @@ exports.getUserById = (req, res, next, id) => {
                 error: "No user Exist"
             })
         }
-
         req.profile = user
         next()
     })
@@ -18,20 +17,97 @@ exports.getUserById = (req, res, next, id) => {
 // getuser controller to get user
 exports.getUser = (req, res) => {
     //TODO: get back here for password
+    req.profile.salt = undefined
+    req.profile.encry_password = undefined
     return res.json(req.profile)
 }
 
-// getAllUsers controller
 
-exports.getAllUser = (req, res) => {
-    User.find().exec((err, users) =>{
-        if(err || !users){
-            return res.status(401).json({
-                error : "No user found"
+// update the user
+
+exports.updateUser = (req, res) => {
+    User.findByIdAndUpdate(
+       { _id : req.profile._id},
+       { $set : req.body },
+       { new : true, useFindAndModify : false},
+       (err, user) => {
+           if(err){
+               return res.status(400).json({
+                   error : "No Updation made"
+               })
+           }
+           user.salt = undefined
+           user.encry_password = undefined
+           res.json({
+               user
+           })
+       }
+    )
+}
+
+
+// read more about this one check for populate also
+exports.userPurchaseList = (req, res) => {
+    Order.find({ user : req.profile._id})
+    .populate("user","_id name")   // check the populate
+    .exec((err, order) => {
+        if(err){
+            return res.status(400).json({
+                error : "No order in this account"
             })
         }
-        return res.json({
-            users
-        })
+
+        return res.json(order)
     })
 }
+
+
+// custome middleware to add purchases list to user
+
+exports.putOrderInPurchaseList = (req, res, next) => {
+    let purchases = []
+    req.body.order.products.map( product => {
+        purchases.push({
+            _id : product._id,
+            name : product.name,
+            description : product.description,
+            category : product.category,
+            quantity : product.quantity,
+            amount : req.body.order.amount,
+            transaction_id : req.body.order.transaction_id
+        })
+    })
+
+    // store the purchases in db
+    User.findOneAndUpdate(
+        { _id : req.profile._id},
+        { $push : {purchases : purchases}},
+        { new : true},   // send the updated object back
+        (err, purchases) => {
+            if(err){
+                return res.status(400).json({
+                    error : " Unable to save purchases list"
+                })
+            }
+        }
+    )
+
+    next()
+}
+
+
+// getAllUsers controller
+
+// exports.getAllUser = (req, res) => {
+//     User.find().exec((err, users) =>{
+//         if(err || !users){
+//             return res.status(401).json({
+//                 error : "No user found"
+//             })
+//         }
+//         return res.json({
+//             users
+//         })
+//     })
+// }
+
--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,16 +1,22 @@
 const express = require("express")
 const router = express.Router()
 
-const { getUserById, getUser, getAllUser} = require("../controllers/user")
+const { getUserById, getUser, updateUser , userPurchaseList ,getAllUser} = require("../controllers/user")
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth")
 
 // middleware route... when a url with parameter :userid hit this route will be called automatically first then the "user/:userid" get request hit
 // so it will also attatch to the req.profile
-router.param("userid", getUserById)
+router.param("userId", getUserById)
 
-router.get("/user/:userid", isSignedIn, isAuthenticated ,getUser)
+router.get("/user/:userId", isSignedIn, isAuthenticated ,getUser)
 
-// get all users
-router.get("/users",  getAllUser)
+router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser)
+
+router.put("/orders/user/:userId", isSignedIn, isAuthenticated, userPurchaseList)
+
+
+
+// // get all users
+// router.get("/users",  getAllUser)
 
 module.exports = router
